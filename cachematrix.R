079b6ceef56c7d9c4d414329167d54de31275e13
smanduri <mandurisrilakshmi98@gmail.com>
2021-05-26 17:59:09 +0530

    Coursera Programming Assignment Week 3

--- cachematrix.R
+++ cachematrix.R
@@ -2,8 +2,24 @@
 ## functions do
 
 ## Write a short comment describing this function
+library(MASS)
 
 makeCacheMatrix <- function(x = matrix()) {
+  
+  invmatrix<-NULL            
+  set<-function(y){
+    x<<-y
+    matrix<<-NULL
+  }
+  get<-function()x            
+  setinv<-function(inverse)  invmatrix<<-inverse
+  getinv<-function(){ 
+    inver<-ginv(x)
+    inver%*%x          
+  }
+  list(set = set, get = get, 
+       setinv = setinv, 
+       getinv = getinv)
 
 }
 
@@ -12,4 +28,15 @@ makeCacheMatrix <- function(x = matrix()) {
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+  inv<-x$getinv()                  
+  if(!is.null(inv)){                 
+    message("Received Cached Data!")
+    return(inv)
+  }                      
+  
+  data<-x$get()
+  matrix<-solve(data,...)             
+  x$setinv(inv)
+  inv   ## Return a matrix that is the inverse of 'x'
 }
+
